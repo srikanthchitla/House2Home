3f8c17fd04b332127af9072dddfdfd479c28d1d7
srikanthchitla <srikanth.chitla@accenture.com>
2017-11-29 22:28:18 +0530

    TC

--- House2Home/js/jquery.js
+++ House2Home/js/jquery.js
@@ -6,3 +6,28 @@
         $(this).siblings("li").removeClass("current");
     });
 });
+
+  $(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 500);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');
+
+
+       
+
+});
+
